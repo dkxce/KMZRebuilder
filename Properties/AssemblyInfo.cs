2d0f44688cdd649b5271f7590e76e7a8f36e8d6c
dkxce <25371256+dkxce@users.noreply.github.com>
2022-02-04 17:17:36 +0300

    b129
    * NaviMapNet.dll updated
    * More GPI compatibility

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("22.2.3.127")]
-[assembly: AssemblyFileVersion("22.2.3.127")]
+[assembly: AssemblyVersion("22.2.4.129")]
+[assembly: AssemblyFileVersion("22.2.4.129")]
